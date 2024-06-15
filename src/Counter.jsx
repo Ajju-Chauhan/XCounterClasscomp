166289faee0d75a406db7d1ecbbafbaacdf2b522
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-15 14:51:45 +0530

    commit code...

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,27 +1,31 @@
 import React from "react";
+
 class Counter extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {count:0};
-      this.increment = this.increment.bind(this);
-      this.decrement = this.decrement.bind(this);
-    }
-  
-    increment() {
-      this.setState({ count: this.state.count + 1 });
-    }
-    decrement() {
-      this.setState({ count: this.state.count - 1 });
-    }
-    render() {
-      return (
-        <>
-        <h1>Counter App</h1>
-       <h5> Count : {this.state.count}</h5>
-       <button onClick={this.increment}>Increment</button>
-       <button onClick={this.decrement} >Decrement</button>
-        </>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
-  export default Counter;
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <h5>Count: {this.state.count}</h5>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
