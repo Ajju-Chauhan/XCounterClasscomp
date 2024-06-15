438f86f3387097849aee065ffcd42f091455946e
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2024-06-15 14:29:13 +0530

    class cop push

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {count:0};
+      this.increment = this.increment.bind(this);
+      this.decrement = this.decrement.bind(this);
+    }
+  
+    increment() {
+      this.setState({ count: this.state.count + 1 });
+    }
+    decrement() {
+      this.setState({ count: this.state.count - 1 });
+    }
+    render() {
+      return (
+        <>
+        <h1>Counter App</h1>
+       <h5> Count : {this.state.count}</h5>
+       <button onClick={this.increment}>Incremant</button>
+       <button onClick={this.decrement} >Decremant</button>
+        </>
+      );
+    }
+  }
+  export default Counter;
